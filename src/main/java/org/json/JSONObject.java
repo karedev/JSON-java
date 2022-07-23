bf35345dc4fb2b97a56241e17c3781390943921a
omerh <omerhalit@gmail.com>
2022-07-24 00:07:20 +0300

    Streaming added.

--- src/main/java/org/json/JSONObject.java
+++ src/main/java/org/json/JSONObject.java
@@ -1,31 +1,6 @@
 package org.json;
 
 import java.io.Closeable;
-
-/*
- Copyright (c) 2002 JSON.org
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- The Software shall be used for Good, not Evil.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
-
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -48,6 +23,7 @@ import java.util.Map.Entry;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * A JSONObject is an unordered collection of name/value pairs. Its external
@@ -2685,6 +2661,16 @@ public class JSONObject {
             results.put(entry.getKey(), value);
         }
         return results;
+    }
+    
+    /**
+     * Creates a stream from the underlying
+     * map instance of this object.
+     * 
+     * @return Stream
+     */
+    public Stream<Entry<String, Object>> stream() {
+        return map.entrySet().stream();
     }
 
     /**
