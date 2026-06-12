f2f213cbf798d549f075cb5a778002aaeeaa61e1
Omer Halit <omerhalit@kareku.com>
2026-06-13 02:23:26 +0300

    feat: domain update

---
+++ src/test/java/com/kareku/json/junit/data/MyJsonString.java
@@ -0,0 +1,14 @@
+package com.kareku.json.junit.data;
+
+import com.kareku.json.JSONString;
+
+/**
+ * Used in testing when a JSONString is needed
+ */
+public class MyJsonString implements JSONString {
+
+    @Override
+    public String toJSONString() {
+        return "my string";
+    }
+}
